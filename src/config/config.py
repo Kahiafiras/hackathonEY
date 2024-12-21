6b7dffcb90285fc8d24e6c82e577d9092ec5255b
Firaskahia <fkahia@yellowsys.fr>
2024-12-21 22:35:55 +0100

    feat:add class basesettings

---
+++ src/config/config.py
@@ -0,0 +1,18 @@
+"""Module to call All configs"""
+from functools import lru_cache
+import os
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    """Class to call all configs"""
+    API_key: str
+  
+@lru_cache()
+def get_settings():
+    environment = os.getenv("ENVIRONMENT", "local")
+    if environment == "local":
+        return Settings(_env_file=".env", _env_file_encoding="utf-8")  # type: ignore
+    elif environment == "production":
+        return Settings()
+    else:
+        raise ValueError("Invalid environment")
